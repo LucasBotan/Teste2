d06ff5054f748478b7bfd48ac16fe539ed217113
Emilio Resende <emilio.resende@faculdadeimpacta.com.br>
2022-10-24 21:41:10 -0300

    add first version

---
+++ src/main/java/fit/App.java
@@ -0,0 +1,20 @@
+package fit;
+
+import fit.domain.Book;
+import fit.services.BookService;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        var bookSvc = new BookService();
+        System.out.println("Dados de livros...");
+
+        bookSvc.add(new Book("Title default", 1));
+        System.out.println(bookSvc.getAll());
+    }
+}
